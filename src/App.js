033385b6f701f66b2719384784abbb7345b657e4
zdbrenda <zdbrenda@gmail.com>
2021-07-25 16:51:28 -0600

    Added basic structure of the e-commerce store

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+ import { Products, Navbar} from './components';
+
+const App = () =>{
+    return (
+        <div>
+            <Navbar />
+            <Products />
+
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {AppBar,  IconButton, Badge, MenuItem, Menu, Typography} from '@material-ui/core';
+import Toolbar from '@material-ui/core/Toolbar';
+
+import {ShoppingCart} from '@material-ui/icons';
+import logo from '../../assets/commerce.png';
+import useStyles from './styles';
+
+
+const NavBar = () => {
+    const classes = useStyles();
+    return (
+        <>
+            <AppBar position="fixed" className = {classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant = "h6" className = {classes.title} color="inherit">
+                        <img src={logo} alt="Brenda's Store" height="25px" className ={classes.image}/>
+                        Brenda's Store
+                    </Typography>
+                    <div className={classes.grow}/>
+                    <div className={classes.button}>
+                        <IconButton aria-label = "Show cart items" color="inherit">
+                            <Badge badgeContent={2} color="secondary">
+                                <ShoppingCart />
+
+                                </Badge>
+
+                        </IconButton>
+                    </div>
+                </Toolbar>
+
+            </AppBar>
+        </>
+    )
+}
+
+export default NavBar;
