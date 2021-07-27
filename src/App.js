e2307b1f889bae36be4c0a79f407d4fc328cc64b
zdbrenda <zdbrenda@gmail.com>
2021-07-27 13:36:48 -0600

    Added full functionality for shopping cart

--- src/App.js
+++ src/App.js
@@ -19,10 +19,28 @@ const App = () =>{
 
     const handleAddToCart = async(productId,quantity) =>{
 
-        const item = await commerce.cart.add(productId, quantity);
-        setCart(item.cart);
+        const { cart } = await commerce.cart.add(productId, quantity);
+        setCart( cart );
 
 
+    }
+
+    const handleUpdateCartQty = async(productId, quantity) =>{
+        const { cart } = await commerce.cart.update(productId, {quantity});
+        setCart( cart );
+    }
+
+    const handleRemoveFromCart = async (productId) =>{
+        const { cart } = await commerce.cart.remove(productId);
+
+        setCart(cart);
+
+    }
+
+    const handleEmptyCart = async() =>{
+
+        const { cart }  = await commerce.cart.empty();
+        setCart(cart);
     }
 
     useEffect(()=>{
@@ -41,7 +59,11 @@ const App = () =>{
                         <Products products={products} onAddToCart={handleAddToCart} />
                     </Route>
                     <Route exact path="/cart">
-                        {cart.id? <Cart cart={cart}/> : null}
+                        <Cart cart={cart}
+                        handleUpdateCartQty={handleUpdateCartQty}
+                        handleRemoveFromCart={handleRemoveFromCart}
+                        handleEmptyCart={handleEmptyCart} /> 
+
                     </Route>
                 </Switch>
             </div>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
     const classes = useStyles();
     
     const EmptyCart = () =>(
@@ -18,7 +18,8 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item)=>(
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <CartItem item={item} />
+                        <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} 
+                        onRemoveFromCart={handleRemoveFromCart}/>
 
                     </Grid>
 
@@ -29,7 +30,8 @@ const Cart = ({ cart }) => {
                 <Typography variant="h4">
                     Subtotal: { cart.subtotal.formatted_with_symbol }
                     <div>
-                        <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">
+                        <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary"
+                        onClick={handleEmptyCart}>
                             Empty Cart
                         </Button>
                         <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">
